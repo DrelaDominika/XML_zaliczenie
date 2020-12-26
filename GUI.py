e63c734e26fb349ed7769bdfbad8df05405224ef
Przemysław Stachurski <przemekstachurski737@gmail.com>
2020-12-26 20:26:09 +0100

    i rest my case

--- GUI.py
+++ GUI.py
@@ -12,7 +12,7 @@ filepath = ''  # var for XML file path
 classA = False
 classB = False
 listaUczniow = []
-numer=0
+numer = 0
 
 canvas = tk.Canvas(height=400, width=400)
 canvas.pack()
@@ -33,9 +33,8 @@ def write_XML():
     global filepath
     global lista
     global numer
-    file = r'/szablon.xml'
-    tree2 = ET.parse(file)
-    rootParser = tree2.getroot()
+    global classA
+    global classB
 
     numer = int(students_entry.get())  # pobranie numeru z aplikacji
     imie = listaUczniow[numer][1]
@@ -73,40 +72,29 @@ def write_XML():
     sredniaTag = ET.SubElement(uczenTag, 'srednia')
     sredniaTag.text = str(srednia)
 
-    raw = ET.tostring(uczenTag, encoding='unicode')
+    if ((numer in range(1, 10) and classA) or (numer in range(11, 20) and classB)):
+        raw = ET.tostring(uczenTag, encoding='unicode')  # zapisywanie xmla do pliku
+        pretty = minidom.parseString(raw).toprettyxml(indent='    ')
+        with open(imie + ' ' + nazwisko, 'w') as f:
+            f.write(pretty)
+    else:
+        error_label.configure(text='BRAK UCZNIA O PODANYM NUMERZE W DANEJ KLASIE', fg='red')
 
-    pretty = minidom.parseString(raw).toprettyxml(indent='    ')
 
-    with open(imie + ' ' + nazwisko, 'w') as f:
-        f.write(pretty)
-
-
-def parse_XML():
-    global listaUczniow
+def display_info():
     global filepath
     global lista
+    global numer
+    global listaUczniow
+    global classA
+    global classB
+
     tree = ET.parse(filepath)
     rootParser = tree.getroot()
 
     Parser.generating_lists(rootParser)
 
     listaUczniow = Parser.collecting_data()
-    # for e in listaUczniow:
-    #    print(e)
-    # tablica dwuwymiarowa dla każdego ucznia imie,naziwsko,polski.avg,matematyka.avg.....
-    # find uczen where id_ucznia = x (wczytanie z gui od 1 do 20
-    # write uczen.imie, uczen.nazwisko
-    # write uczen/przedmioty/polski/oceny .split(,) .avg()
-    # write uczen/przedmioty/polski/angielski ....
-
-    # for uczen in rootParser.iter('przedmioty'):
-    #     for el in rootParser.iter('oceny'):
-    #         print(przedmioty.text)
-
-
-def display_info():
-    global numer
-
 
     numer = int(students_entry.get())  # pobranie numeru z aplikacji
     imie = listaUczniow[numer][1]
@@ -118,25 +106,32 @@ def display_info():
     geografia = listaUczniow[numer][7]
     srednia = listaUczniow[numer][8]
 
-    info_label.config(text="Średnie ucznia o imieniu: {} {}/n"
-                           "j. polski:      {} /n"
-                           "j. angielski:   {} /n"
-                           "matematyka:     {} /n"
-                           "fizyka:         {} /n"
-                           "geografia:      {} /n"
-                           "srednia ogólna: {}"
-                           ).format(imie,nazwisko,polski,angielski,matematyka,fizyka,geografia,srednia)
+    if ((numer in range(1, 10) and classA) or (numer in range(11, 20) and classB)):
+        info_label.config(text=f"Średnie ucznia o imieniu: {str(imie)} {str(nazwisko)} \n"  # displaying info in label
+                               f"j. polski:      {str(polski)} \n"
+                               f"j. angielski:   {str(angielski)} \n"
+                               f"matematyka:     {str(matematyka)} \n"
+                               f"fizyka:         {str(fizyka)} \n"
+                               f"geografia:      {str(geografia)} \n"
+                               f"srednia ogólna: {str(srednia)}"
+                          )
+    else:
+        error_label.configure(text='BRAK UCZNIA O PODANYM NUMERZE W DANEJ KLASIE', fg='red')
 
 
 def checkboxA():
     global classA
-    classA = not classA
+    global classB
+    classA = True
+    classB = False
     students_checkbox2.deselect()
 
 
 def checkboxB():
     global classB
-    classB = not classB
+    global classA
+    classB = True
+    classA = False
     students_checkbox1.deselect()
 
 
@@ -168,13 +163,16 @@ students_label.grid(row=1, column=2)
 students_entry = tk.Entry(frame_students)
 students_entry.grid(row=2, column=2)
 
-students_button = tk.Button(frame_students, text='Wyświetl dane',command =display_info)
+students_button = tk.Button(frame_students, text='Wyświetl dane', command=display_info)
 students_button.place(relx=0.7, rely=0.03, relheight=0.45, relwidth=0.28)
 
 students_button2 = tk.Button(frame_students, text='Eksportuj dane', command=write_XML)
 students_button2.place(relx=0.7, rely=0.5, relheight=0.45, relwidth=0.28)
 
-info_label = tk.Label(root, bg='white', text='',borderwidth=2, relief='ridge') # label for displaying chosen student`s info
+info_label = tk.Label(root, bg='white', text='', borderwidth=2,
+                      relief='ridge')  # label for displaying chosen student`s info
 info_label.place(relwidth=0.9, relheight=0.5, relx=0.05, rely=0.45)
 
+error_label = tk.Label(root, text='', justify='center')
+error_label.place(relwidth=0.9, relheight=0.06, relx=0.05, rely=0.39)
 root.mainloop()  # keep as the last line
